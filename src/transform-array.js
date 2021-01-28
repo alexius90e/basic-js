19625a4a1d8c20f9c242d8167d6a1ea03d85a6a5
alexius90e <alexius90e@mail.ru>
2021-01-28 23:02:51 +0300

    transform array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw "Error";
+  if (arr.length === 0) return [];
+  if (
+    !arr.includes("--discard-prev") &&
+    !arr.includes("--double-prev") &&
+    !arr.includes("--double-next") &&
+    !arr.includes("--discard-next")
+  ) {
+    return arr;
+  }
+  let result = [];
+  arr.forEach((item, index, arr) => {
+    if (item === "--discard-prev") {
+      if (index === 0) {
+        result.push(null);
+      } else {
+        result.pop();
+      }
+    } else if (item === "--discard-next") {
+      result.push(null);
+    } else if (arr[index - 1] && arr[index - 1] === "--discard-next") {
+      result.push(null);
+    } else if (item === "--double-next") {
+      if (index === arr.length - 1) {
+        result.push(null);
+      } else {
+        result.push(arr[index + 1]);
+      }
+    } else if (item === "--double-prev") {
+      if (index === 0) {
+        result.push(null);
+      } else {
+        result.push(result[result.length-1]);
+      }
+    } else {
+      result.push(item);
+    }
+  });
+  return result.filter((item) => item !== null);
 };
