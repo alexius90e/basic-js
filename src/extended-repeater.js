4680c49de61b356e68e144cad72920a19ee56250
alexius90e <alexius90e@mail.ru>
2021-01-28 19:18:48 +0300

    reapeat counters  done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = str + '';
+  let separator = options.hasOwnProperty('separator') ? options.separator + '' : '+';
+  let repeatTimes = options.repeatTimes ? +options.repeatTimes : 1;
+  let addition = options.hasOwnProperty('addition') ? options.addition + '' : '';
+  let additionRepeatTimes = options.additionRepeatTimes ? +options.additionRepeatTimes : 1;
+  let additionSeparator = options.hasOwnProperty('additionSeparator') ? options.additionSeparator + '' : '|';
+  addition = Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+  return Array(repeatTimes).fill(str + addition).join(separator);
 };
   
