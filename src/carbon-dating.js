2aafea1353c6447cd3632192bb138470b3198d50
alexius90e <alexius90e@mail.ru>
2021-01-28 16:36:14 +0300

    carbone done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,14 +4,16 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  console.log(sampleActivity);
-  if (typeof sampleActivity !== 'string') return false;
+  if (typeof sampleActivity !== "string") return false;
   if (isNaN(+sampleActivity)) return false;
-  if (isFinite(+sampleActivity)) return false;
-  if (!sampleActivity || sampleActivity === undefined || sampleActivity > MODERN_ACTIVITY){
+  //   if (isFinite(+sampleActivity)) return false;
+  if (sampleActivity === undefined) return false;
+  if (+sampleActivity > 0 && +sampleActivity < MODERN_ACTIVITY) {
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let t = Math.log(MODERN_ACTIVITY / +sampleActivity) / k;
+    console.log(Math.floor(t));
+    return Math.floor(t);
+  } else {
     return false;
   }
-  let k = 0.693 / HALF_LIFE_PERIOD;
-  let t = Math.log(MODERN_ACTIVITY / +sampleActivity) / k;
-  return t;
 };
