0090f1611dc5c3820a3d34d7361c51a18a984c38
alexius90e <alexius90e@mail.ru>
2021-01-30 21:26:51 +0300

    cryptography  done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.letters = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined){
+      throw new Error();
+    }
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase()
+    .repeat(Math.ceil(message.length / key.length))
+    .slice(0, message.length)
+    .split('');
+    let diff = 0;
+    let result = message.map((item, index) => {
+      if (this.letters.includes(item)){
+        return this.letters[(this.letters.indexOf(item) + this.letters.indexOf(key[index - diff])) % this.letters.length];
+      } else {
+        diff += 1;
+        return item;
+      }
+    })
+    return this.isDirect ? result.join('') : result.reverse().join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    if (message === undefined || key === undefined){
+      throw new Error();
+    }
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase()
+    .repeat(Math.ceil(message.length / key.length))
+    .slice(0, message.length)
+    .split('');
+    let diff = 0;
+    let result = message.map((item, index) => {
+      if (this.letters.includes(item)){
+        return this.letters[(this.letters.indexOf(item) + this.letters.length - this.letters.indexOf(key[index - diff])) % this.letters.length];
+      } else {
+        diff += 1;
+        return item;
+      }
+    })
+    return this.isDirect ? result.join('') : result.reverse().join('');
   }
 }
 
